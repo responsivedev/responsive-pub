aad40832ecac8c818296f8abba8c41a54e0cc50c
Rohan Desai <rohan@responsive.dev>
2023-06-26 11:18:40 -0700

    log operator logs to our logging pipeline
    This patch sets operator deployments up to log to our logging pipeline:
    - packages operator with log4j2, which ships with a maintained kafka appender
    - adds a customized login module for authenticating with ccloud brokers. It
      reads username/password from a separate file (rather than the log4j properties
      file). This way we can put the api key/secret in a k8s secret.
    - docker run script sets up a log4j properties file that logs to kafka, if
      configured to do so.
    - helm chart mounts kafka secret and sets up environment variables so that
      log4j with kafka can be used.
    
    Tested using local operator in kind running against a real observability stack"

---
+++ operator/src/main/java/dev/responsive/kafka/auth/PropertiesFileLoginModule.java
@@ -0,0 +1,70 @@
+package dev.responsive.kafka.auth;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+import javax.security.auth.Subject;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.spi.LoginModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class PropertiesFileLoginModule implements LoginModule {
+  private static final Logger LOGGER = LoggerFactory.getLogger(PropertiesFileLoginModule.class);
+
+  private static final String PROPERTIES_FILE = "properties";
+  private static final String USERNAME_CONFIG = "username";
+  private static final String PASSWORD_CONFIG = "password";
+
+  @Override
+  public void initialize(
+      final Subject subject,
+      final CallbackHandler callbackHandler,
+      final Map<String, ?> sharedState,
+      final Map<String, ?> options
+  ) {
+    final String propertiesPath = (String) options.get(PROPERTIES_FILE);
+    if (propertiesPath == null) {
+      LOGGER.info("no properties file specified in JAAS config");
+      return;
+    }
+    final Properties properties = new Properties();
+    try (InputStream in = new FileInputStream(propertiesPath)) {
+      properties.load(in);
+    } catch (final IOException e) {
+      LOGGER.info("error loading properties file", e);
+    }
+    String username = properties.getProperty(USERNAME_CONFIG);
+    if (username != null) {
+      LOGGER.info("login using name " + username);
+      subject.getPublicCredentials().add(username);
+    }
+    String password = properties.getProperty(PASSWORD_CONFIG);
+    if (password != null) {
+      subject.getPrivateCredentials().add(password);
+    }
+  }
+
+  @Override
+  public boolean login() {
+    return true;
+  }
+
+  @Override
+  public boolean logout() {
+    return true;
+  }
+
+  @Override
+  public boolean commit() {
+    return true;
+  }
+
+  @Override
+  public boolean abort() {
+    return false;
+  }
+}
