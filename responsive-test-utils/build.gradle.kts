b41c7ce3eed8c2efe8473815bc6350f0948a4b72
Almog Gavra <almog.gavra@gmail.com>
2023-09-06 14:20:43 -0700

    revert pinned dependency

--- responsive-test-utils/build.gradle.kts
+++ responsive-test-utils/build.gradle.kts
@@ -19,12 +19,9 @@ plugins {
 }
 
 dependencies {
-    // TODO: we should make sure to fail the release of kafka-client
-    // if we don't bump this up - or have some other mechanism to
-    // ensure they are released in tandem - otherwise it is possible
-    // that the latest release of kafka-client will be untestable
-    implementation(libs.bundles.scylla)
-    implementation("dev.responsive:kafka-client:0.7.1")
+    // TODO: we should make sure to never release this unless the kafka-client
+    // version is pinned
+    implementation(project(":kafka-client"))
 
     implementation(libs.bundles.scylla)
     implementation(libs.kafka.streams.test.utils)
