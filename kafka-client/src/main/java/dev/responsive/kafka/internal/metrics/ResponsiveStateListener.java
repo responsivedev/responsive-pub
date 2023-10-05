db9949cfa0188b132958d501b43a46c312aeb195
A. Sophie Blee-Goldman <sophie@responsive.dev>
2023-10-04 21:51:10 -0700

    add headers

--- kafka-client/src/main/java/dev/responsive/kafka/internal/metrics/ResponsiveStateListener.java
+++ kafka-client/src/main/java/dev/responsive/kafka/internal/metrics/ResponsiveStateListener.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Responsive Computing, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.responsive.kafka.internal.metrics;
 
 import static dev.responsive.kafka.internal.metrics.ApplicationMetrics.STREAMS_STATE;
@@ -17,12 +33,13 @@ public class ResponsiveStateListener implements StateListener, Closeable {
 
   public ResponsiveStateListener(final ResponsiveMetrics metrics) {
     this.metrics = metrics;
-    this.stateMetric = metrics.metricName(
+
+    stateMetric = metrics.metricName(
         STREAMS_STATE,
         STREAMS_STATE_DESCRIPTION,
-        metrics.applicationLevelMetric(),
-        (config, now) -> currentState.ordinal()
+        metrics.applicationLevelMetric()
     );
+    metrics.addMetric(stateMetric, (config, now) -> currentState.ordinal());
   }
 
   public void registerUserStateListener(final StateListener userStateListener) {
