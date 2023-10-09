0aa24e3ca8e9a96c0785ce8fc29b18dda6693a33
A. Sophie Blee-Goldman <sophie@responsive.dev>
2023-10-09 12:55:10 -0700

    Set Kafka version to 3.6.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,7 +44,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             version("jackson", "2.14.2")
-            version("kafka", "3.4.0")
+            version("kafka", "3.6.0")
             version("scylla", "4.15.0.0")
             version("javaoperatorsdk", "4.3.0")
             version("grpc", "1.52.1")
