b71f862e42f7e95bad061ee6c41abfde2ce04df1
Antoine Pourchet <antoine@antoine-mbp-responsive.local>
2024-02-27 09:59:20 -0700

    Added tests for SessionKeySpec decoding

--- kafka-client/src/main/java/dev/responsive/kafka/internal/db/SessionKeySpec.java
+++ kafka-client/src/main/java/dev/responsive/kafka/internal/db/SessionKeySpec.java
@@ -16,7 +16,10 @@
 
 package dev.responsive.kafka.internal.db;
 
+import static org.apache.kafka.streams.state.StateSerdes.TIMESTAMP_SIZE;
+
 import dev.responsive.kafka.internal.utils.SessionKey;
+import java.nio.ByteBuffer;
 import java.util.function.Predicate;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
@@ -36,13 +39,38 @@ public class SessionKeySpec implements KeySpec<SessionKey> {
   @Override
   public SessionKey keyFromRecord(final ConsumerRecord<byte[], byte[]> record) {
     final byte[] key = record.key();
-
-    // TODO(antoine): IMPLEMENT using kafka's SessionKeySchema
-    throw new UnsupportedOperationException("Not yet implemented.");
+    return new SessionKey(
+        extractKeyBytes(key),
+        extractStartTimestamp(key),
+        extractEndTimestamp(key)
+    );
   }
 
   @Override
   public boolean retain(final SessionKey key) {
     return withinRetention.test(key);
+  }
+
+  /*
+   * Inspired from KafkaStream internal functions within SessionKeySchema.
+   */
+  private static byte[] extractKeyBytes(final byte[] binaryKey) {
+    final byte[] bytes = new byte[binaryKey.length - 2 * TIMESTAMP_SIZE];
+    System.arraycopy(binaryKey, 0, bytes, 0, bytes.length);
+    return bytes;
+  }
+
+  /*
+   * Inspired from KafkaStream internal functions within SessionKeySchema.
+   */
+  private static long extractEndTimestamp(final byte[] binaryKey) {
+    return ByteBuffer.wrap(binaryKey).getLong(binaryKey.length - 2 * TIMESTAMP_SIZE);
+  }
+
+  /*
+   * Inspired from KafkaStream internal functions within SessionKeySchema.
+   */
+  private static long extractStartTimestamp(final byte[] binaryKey) {
+    return ByteBuffer.wrap(binaryKey).getLong(binaryKey.length - TIMESTAMP_SIZE);
   }
 }
