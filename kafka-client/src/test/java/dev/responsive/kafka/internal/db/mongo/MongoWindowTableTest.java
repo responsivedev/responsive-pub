aeb77b904ca1097267ec3f3f1f1ac28ccf5421ab
Antoine Pourchet <antoine@antoine-mbp-responsive.local>
2024-02-27 13:25:49 -0700

    added ranged fetch test

--- kafka-client/src/test/java/dev/responsive/kafka/internal/db/mongo/MongoWindowTableTest.java
+++ kafka-client/src/test/java/dev/responsive/kafka/internal/db/mongo/MongoWindowTableTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 Responsive Computing, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.responsive.kafka.internal.db.mongo;
 
 import static dev.responsive.kafka.api.config.ResponsiveConfig.STORAGE_HOSTNAME_CONFIG;
@@ -10,15 +26,17 @@ import dev.responsive.kafka.internal.utils.SessionUtil;
 import dev.responsive.kafka.internal.utils.WindowedKey;
 import dev.responsive.kafka.testutils.ResponsiveConfigParam;
 import dev.responsive.kafka.testutils.ResponsiveExtension;
+import java.util.ArrayList;
 import java.util.Map;
 import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KeyValue;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-class MongoWindowedTableTest {
+class MongoWindowTableTest {
 
   @RegisterExtension
   public static final ResponsiveExtension EXT = new ResponsiveExtension(StorageBackend.MONGO_DB);
@@ -72,5 +90,40 @@ class MongoWindowedTableTest {
     assertThat(value, Matchers.nullValue());
     value = table.fetch(0, Bytes.wrap("other".getBytes()), 0);
     assertThat(value, Matchers.nullValue());
+  }
+
+  @Test
+  public void shouldSucceedRangeSetGet() {
+    // Given:
+    final WindowSegmentPartitioner partitioner = new WindowSegmentPartitioner(10_000L, 1_000L);
+    final var segment = partitioner.segmenter().activeSegments(0, 100).get(0);
+
+    final MongoWindowedTable table = new MongoWindowedTable(client, name, partitioner,
+        false, UNSHARDED
+    );
+    final var flushManager = table.init(0);
+    flushManager.updateOffsetAndStreamTime(0, 100);
+    flushManager.createSegment(segment);
+
+    // When:
+    final var byteKey = Bytes.wrap("key".getBytes());
+    final var windowedKey = new WindowedKey(byteKey, 500);
+    var writer = flushManager.createWriter(segment);
+    writer.insert(
+        windowedKey,
+        DEFAULT_VALUE,
+        table.localEpoch(0)
+    );
+    writer.flush();
+
+    // Then:
+    var it = table.fetch(0, byteKey, 100, 600);
+    var kvs = new ArrayList<KeyValue<WindowedKey, byte[]>>();
+    it.forEachRemaining(kvs::add);
+
+    assertThat(kvs, Matchers.hasSize(1));
+    assertThat(kvs.get(0).key.key, Matchers.equalTo(windowedKey.key));
+    assertThat(kvs.get(0).key.windowStartMs, Matchers.equalTo(windowedKey.windowStartMs));
+    assertThat(kvs.get(0).value, Matchers.equalTo(DEFAULT_VALUE));
   }
 }
