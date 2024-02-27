eac53bc0d079d5d1981ef40eae9ec0b5bc84d166
Almog Gavra <almog.gavra@gmail.com>
2024-02-26 18:42:36 -0800

    grumble grumble checkstyle

--- kafka-client/src/test/java/dev/responsive/kafka/internal/stores/CommitBufferTest.java
+++ kafka-client/src/test/java/dev/responsive/kafka/internal/stores/CommitBufferTest.java
@@ -78,7 +78,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
