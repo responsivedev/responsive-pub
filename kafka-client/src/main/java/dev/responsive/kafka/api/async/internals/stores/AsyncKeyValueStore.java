518b2fec5ac0eda9e437b6826d833889317500e4
Rohan Desai <rohan@responsive.dev>
2024-04-18 23:25:04 -0700

    review feedback

--- kafka-client/src/main/java/dev/responsive/kafka/api/async/internals/stores/AsyncKeyValueStore.java
+++ kafka-client/src/main/java/dev/responsive/kafka/api/async/internals/stores/AsyncKeyValueStore.java
@@ -18,7 +18,7 @@ package dev.responsive.kafka.api.async.internals.stores;
 
 import dev.responsive.kafka.api.async.internals.contexts.AsyncThreadProcessorContext;
 import dev.responsive.kafka.api.async.internals.contexts.AsyncUserProcessorContext;
-import dev.responsive.kafka.api.async.internals.contexts.MergedProcessorContext;
+import dev.responsive.kafka.api.async.internals.contexts.DelayedAsyncStoreWriter;
 import dev.responsive.kafka.api.async.internals.events.AsyncEvent;
 import dev.responsive.kafka.api.async.internals.events.DelayedWrite;
 import java.util.List;
@@ -53,19 +53,19 @@ public class AsyncKeyValueStore<KS, VS> implements KeyValueStore<KS, VS> {
   private final Logger log;
 
   private final KeyValueStore<KS, VS> userDelegate;
-  private final AsyncUserProcessorContext<?, ?> userProcessorContext;
+  private final DelayedAsyncStoreWriter delayedWriter;
 
   @SuppressWarnings("unchecked")
   public AsyncKeyValueStore(
       final String name,
       final int partition,
       final KeyValueStore<?, ?> userDelegate,
-      final AsyncUserProcessorContext<?, ?> userProcessorContext
+      final DelayedAsyncStoreWriter delayedWriter
   ) {
     this.log = new LogContext(String.format(" async-store [%s-%d]", name, partition))
         .logger(AsyncKeyValueStore.class);
     this.userDelegate = (KeyValueStore<KS, VS>) userDelegate;
-    this.userProcessorContext = userProcessorContext;
+    this.delayedWriter = delayedWriter;
   }
 
   public void executeDelayedWrite(final DelayedWrite<KS, VS> delayedWrite) {
@@ -83,24 +83,7 @@ public class AsyncKeyValueStore<KS, VS> implements KeyValueStore<KS, VS> {
    */
   @Override
   public void put(KS key, VS value) {
-    final Thread currentThread = Thread.currentThread();
-    final MergedProcessorContext<?, ?> rawThreadCtx = userProcessorContext.delegate();
-    if (rawThreadCtx instanceof AsyncThreadProcessorContext) {
-      // todo: yuck
-      final AsyncThreadProcessorContext<?, ?> threadCtx = (AsyncThreadProcessorContext<?, ?>)
-          userProcessorContext.delegate();
-      final AsyncEvent currentAsyncEvent = threadCtx.currentAsyncEvent();
-
-      currentAsyncEvent.addWrittenRecord(new DelayedWrite<>(key, value, name()));
-    } else {
-      log.error("A non-AsyncThread invoked put on this async store. Caller thread name was {}",
-                currentThread.getName());
-      // The most common reason this case might be hit, besides a bug in the async framework itself,
-      // is if users attempt to insert into a state store from other Processor methods, like
-      // #init or #close
-      throw new IllegalStateException("Can only call #put on an async state store inside the "
-                                          + "#process method of an async processor");
-    }
+    delayedWriter.acceptDelayedWriteToAsyncStore(new DelayedWrite<>(key, value, name()));
   }
 
   @Override
