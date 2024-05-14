76db3d257455f88682fb12ebb059382e63c95f28
Rohan Desai <rohan@responsive.dev>
2024-05-14 09:52:22 -0700

    deploy 2 app nodes instead of using replicas config

--- kafka-client/build.gradle.kts
+++ kafka-client/build.gradle.kts
@@ -16,7 +16,7 @@ import java.io.ByteArrayOutputStream
  * limitations under the License.
  */
 
-// counter to change sha: 1
+// counter to change sha: 2
 
 plugins {
     id("responsive.java-library-conventions")
