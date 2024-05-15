5b808086cf0ee584ebff21c1d887e0ce2227a94d
Rohan Desai <rohan@responsive.dev>
2024-05-14 17:12:05 -0700

    try kraft kafka

--- kafka-client/build.gradle.kts
+++ kafka-client/build.gradle.kts
@@ -16,7 +16,7 @@ import java.io.ByteArrayOutputStream
  * limitations under the License.
  */
 
-// counter to change sha: 6
+// counter to change sha: 7
 
 plugins {
     id("responsive.java-library-conventions")
