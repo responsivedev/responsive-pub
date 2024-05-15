c83d15ef13473cf0c6397df81c345d7509fbde42
Rohan Desai <rohan@responsive.dev>
2024-05-15 11:53:01 -0700

    dont mount kafka vols

--- kafka-client/build.gradle.kts
+++ kafka-client/build.gradle.kts
@@ -16,7 +16,7 @@ import java.io.ByteArrayOutputStream
  * limitations under the License.
  */
 
-// counter to change sha: 9
+// counter to change sha: 10
 
 plugins {
     id("responsive.java-library-conventions")
