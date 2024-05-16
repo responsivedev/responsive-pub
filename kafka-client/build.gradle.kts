b22c1b4cdd3bd0818b8f26a78600707368576880
Rohan Desai <rohan@responsive.dev>
2024-05-15 20:21:11 -0700

    try to remove application2

--- kafka-client/build.gradle.kts
+++ kafka-client/build.gradle.kts
@@ -16,7 +16,7 @@ import java.io.ByteArrayOutputStream
  * limitations under the License.
  */
 
-// counter to change sha: 11
+// counter to change sha: 12
 
 plugins {
     id("responsive.java-library-conventions")
