f2b7cca6c9917eff306728020a2d94a962c16684
Rohan Desai <rohan@responsive.dev>
2024-05-16 18:24:10 -0700

    bump sha

--- kafka-client/build.gradle.kts
+++ kafka-client/build.gradle.kts
@@ -16,7 +16,7 @@ import java.io.ByteArrayOutputStream
  * limitations under the License.
  */
 
-// counter to change sha: 14
+// counter to change sha: 15
 
 plugins {
     id("responsive.java-library-conventions")
