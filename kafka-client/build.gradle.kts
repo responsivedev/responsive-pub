08b3bc61fd28992fa4b1c9d04d0a67fcc3e08395
Rohan Desai <rohan@responsive.dev>
2024-05-16 18:04:02 -0700

    use responsive scylla with high timeouts
    revert docker-compose to d29bab1
    switch scylla to responsive-scylla

--- kafka-client/build.gradle.kts
+++ kafka-client/build.gradle.kts
@@ -16,7 +16,7 @@ import java.io.ByteArrayOutputStream
  * limitations under the License.
  */
 
-// counter to change sha: 13
+// counter to change sha: 14
 
 plugins {
     id("responsive.java-library-conventions")
