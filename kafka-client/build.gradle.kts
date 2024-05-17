335da998a24e8e4113b0822b13d7db4f046abc12
Rohan Desai <rohan@responsive.dev>
2024-05-16 21:07:42 -0700

    try dropping repo from compose

--- kafka-client/build.gradle.kts
+++ kafka-client/build.gradle.kts
@@ -16,7 +16,7 @@ import java.io.ByteArrayOutputStream
  * limitations under the License.
  */
 
-// counter to change sha: 15
+// counter to change sha: 16
 
 plugins {
     id("responsive.java-library-conventions")
