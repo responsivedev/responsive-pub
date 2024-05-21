9c0850f0e5980a77dc1c905ae2e8f45094b3ea0a
Rohan Desai <rohan@responsive.dev>
2024-05-21 13:03:32 -0700

    revert all timeout changes

--- kafka-client/build.gradle.kts
+++ kafka-client/build.gradle.kts
@@ -16,7 +16,7 @@ import java.io.ByteArrayOutputStream
  * limitations under the License.
  */
 
-// counter to change sha: 20
+// counter to change sha: 21
 
 plugins {
     id("responsive.java-library-conventions")
