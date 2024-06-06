7b50410a1d1f5773ceaeec19dc80c45e251e623f
Rohan Desai <rohan@responsive.dev>
2024-06-06 16:28:07 -0700

    missing dockerfile change

--- kafka-client/build.gradle.kts
+++ kafka-client/build.gradle.kts
@@ -16,7 +16,7 @@ import java.io.ByteArrayOutputStream
  * limitations under the License.
  */
 
-// counter to change sha: 28
+// counter to change sha: 29
 
 plugins {
     id("responsive.java-library-conventions")
