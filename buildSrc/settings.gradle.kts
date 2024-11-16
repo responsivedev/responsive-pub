a017346fc25aff794b6161ff3ad78e619339737c
Almog Gavra <almog.gavra@gmail.com>
2024-11-15 16:12:35 -0800

    fix typos

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,11 +1,12 @@
 /*
  * Copyright 2024 Responsive Computing, Inc.
  *
- * This source code is licensed under the Responsive Software License Agreement v1.0 available at
+ * This source code is licensed under the Responsive Business Source License Agreement v1.0
+ * available at:
  *
  * https://www.responsive.dev/legal/responsive-bsl-10
  *
  * This software requires a valid Commercial License Key for production use. Trial and commercial
- * licenses can be obtained at https://www.responsive.dev/sdk/get-started.
+ * licenses can be obtained at https://www.responsive.dev
  */
 
