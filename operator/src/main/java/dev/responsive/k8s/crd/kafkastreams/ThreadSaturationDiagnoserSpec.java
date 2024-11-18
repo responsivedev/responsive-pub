1495cb18e7f0a390dabf779b940fd5edc72b7022
Almog Gavra <almog.gavra@gmail.com>
2024-11-18 08:31:41 -0800

    change license to Responsive Business Source License 1.0 (#387)

--- operator/src/main/java/dev/responsive/k8s/crd/kafkastreams/ThreadSaturationDiagnoserSpec.java
+++ operator/src/main/java/dev/responsive/k8s/crd/kafkastreams/ThreadSaturationDiagnoserSpec.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2024 Responsive Computing, Inc.
+ *
+ * This source code is licensed under the Responsive Business Source License Agreement v1.0
+ * available at:
+ *
+ * https://www.responsive.dev/legal/responsive-bsl-10
+ *
+ * This software requires a valid Commercial License Key for production use. Trial and commercial
+ * licenses can be obtained at https://www.responsive.dev
+ */
+
 package dev.responsive.k8s.crd.kafkastreams;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
