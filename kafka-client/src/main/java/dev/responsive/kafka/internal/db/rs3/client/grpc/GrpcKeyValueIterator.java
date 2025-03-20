33800c7c26b09599d85c43fb65b7193b315c5ca6
Jason Gustafson <12502538+hachikuji@users.noreply.github.com>
2025-03-20 10:17:29 -0700

    WIP

--- kafka-client/src/main/java/dev/responsive/kafka/internal/db/rs3/client/grpc/GrpcKeyValueIterator.java
+++ kafka-client/src/main/java/dev/responsive/kafka/internal/db/rs3/client/grpc/GrpcKeyValueIterator.java
@@ -1,10 +1,12 @@
 package dev.responsive.kafka.internal.db.rs3.client.grpc;
 
+import com.google.protobuf.ByteString;
+import dev.responsive.kafka.internal.db.rs3.client.RS3Exception;
+import dev.responsive.kafka.internal.db.rs3.client.RangeBound;
 import dev.responsive.rs3.RS3Grpc;
 import dev.responsive.rs3.Rs3;
 import io.grpc.stub.StreamObserver;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.kafka.common.utils.Bytes;
@@ -14,10 +16,14 @@ import org.apache.kafka.streams.state.KeyValueIterator;
 public class GrpcKeyValueIterator implements KeyValueIterator<Bytes, byte[]> {
   private final RS3Grpc.RS3Stub stub;
   private final Rs3.RangeRequest.Builder requestBuilder;
-  private final BlockingQueue<Rs3.RangeResult> queue;
+  private final BlockingQueue<Message> queue;
+
+  private final AtomicBoolean closed = new AtomicBoolean(false);
   private final AtomicBoolean done = new AtomicBoolean(false);
 
-  private Optional<KeyValue<Bytes, byte[]>> lastKey;
+  private final RangeBound endBound;
+  private RangeBound startBound;
+
 
   @Override
   public boolean hasNext() {
@@ -25,28 +31,50 @@ public class GrpcKeyValueIterator implements KeyValueIterator<Bytes, byte[]> {
     return false;
   }
 
+  public void sendRequest() {
+    final var request = requestBuilder.build();
+
+    stub.range(request, new ScanObserver());
+  }
+
   @Override
   public KeyValue<Bytes, byte[]> next() {
-    if (done.get()) {
+    if (closed.get()) {
+      throw new IllegalStateException();
+    } else if (done.get()) {
       throw new NoSuchElementException();
     } else {
-      final var result = queue.take();
-      if (result.getType() == Rs3.RangeResult.Type.END_OF_STREAM) {
-        done.set(true);
-        throw new NoSuchElementException();
-      } else {
-        final var keyValue = result.getResult();
-        return new KeyValue<>(
-            Bytes.wrap(keyValue.getKey().toByteArray()),
-            keyValue.getValue().toByteArray()
-        );
+      try {
+        final var result = queue.take();
+        return result.map(new Mapper<>() {
+          @Override
+          public KeyValue<Bytes, byte[]> map(final EndOfStream endOfStream) {
+            done.set(true);
+            throw new NoSuchElementException();
+          }
+
+          @Override
+          public KeyValue<Bytes, byte[]> map(final StreamError error) {
+            return null;
+          }
+
+          @Override
+          public KeyValue<Bytes, byte[]> map(final Result result) {
+            final var key = Bytes.wrap(result.key.toByteArray());
+            final var value = result.value.toByteArray();
+            return new KeyValue<>(key, value);
+          }
+        });
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        throw new RS3Exception(e);
       }
     }
   }
 
   @Override
   public void close() {
-
+    this.done.set(true);
   }
 
   @Override
@@ -69,6 +97,45 @@ public class GrpcKeyValueIterator implements KeyValueIterator<Bytes, byte[]> {
     public void onCompleted() {
 
     }
+  }
+
+  private interface Message {
+    <T> T map(Mapper mapper);
+  }
+
+  private static class EndOfStream implements Message {
+    @Override
+    public <T> T map(final Mapper mapper) {
+      return mapper.map(this);
+    }
+  }
+
+  private static class Result implements Message {
+    private final ByteString key;
+    private final ByteString value;
+
+    @Override
+    public <T> T map(final Mapper mapper) {
+      return mapper.map(this);
+    }
+  }
+
+  private static class StreamError implements Message {
+    private final Throwable error;
+    StreamError(final Throwable error) {
+      this.error = error;
+    }
+
+    @Override
+    public <T> T map(final Mapper mapper) {
+      return mapper.map(this);
+    }
+  }
+
+  private interface Mapper<T> {
+    T map(EndOfStream endOfStream);
+    T map(StreamError error);
+    T map(Result result);
   }
 
 }
