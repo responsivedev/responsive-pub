8d47b4d5098f0ae8fb1ad90d9317418040331c98
Jason Gustafson <12502538+hachikuji@users.noreply.github.com>
2025-04-03 19:15:04 -0700

    use more final variables

--- kafka-client/src/main/java/dev/responsive/kafka/internal/db/rs3/RS3WindowedKeySerde.java
+++ kafka-client/src/main/java/dev/responsive/kafka/internal/db/rs3/RS3WindowedKeySerde.java
@@ -6,15 +6,15 @@ import org.apache.kafka.common.utils.Bytes;
 
 public class RS3WindowedKeySerde {
 
-  public byte[] serialize(WindowedKey key) {
-    byte[] result = new byte[key.key.get().length + 8];
-    final ByteBuffer buffer = ByteBuffer.wrap(result);
+  public byte[] serialize(final WindowedKey key) {
+    final var result = new byte[key.key.get().length + 8];
+    final var buffer = ByteBuffer.wrap(result);
     buffer.put(key.key.get());
     buffer.putLong(key.windowStartMs);
     return result;
   }
 
-  public WindowedKey deserialize(byte[] bytes) {
+  public WindowedKey deserialize(final byte[] bytes) {
     final var buffer = ByteBuffer.wrap(bytes);
     final var keyLength = bytes.length - 8;
     final var keyBytes = new byte[keyLength];
