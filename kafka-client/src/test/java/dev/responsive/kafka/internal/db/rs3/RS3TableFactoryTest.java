8f9bfd7c8e149bd7e73003b19849544742bade60
A. Sophie Blee-Goldman <sophie@responsive.dev>
2025-04-09 14:32:50 -0700

    fix test

--- kafka-client/src/test/java/dev/responsive/kafka/internal/db/rs3/RS3TableFactoryTest.java
+++ kafka-client/src/test/java/dev/responsive/kafka/internal/db/rs3/RS3TableFactoryTest.java
@@ -1,27 +1,54 @@
+/*
+ * Copyright 2024 Responsive Computing, Inc.
+ *
+ * This source code is licensed under the Responsive Business Source License Agreement v1.0
+ * available at:
+ *
+ * https://www.responsive.dev/legal/responsive-bsl-10
+ *
+ * This software requires a valid Commercial License Key for production use. Trial and commercial
+ * licenses can be obtained at https://www.responsive.dev
+ */
+
 package dev.responsive.kafka.internal.db.rs3;
 
 import static dev.responsive.kafka.internal.stores.TtlResolver.NO_TTL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import dev.responsive.kafka.api.config.ResponsiveConfig;
+import dev.responsive.kafka.internal.db.rs3.client.CreateStoreOptions;
 import dev.responsive.kafka.internal.db.rs3.client.grpc.GrpcRS3Client;
+import dev.responsive.kafka.internal.db.rs3.client.grpc.GrpcRS3Client.Connector;
 import dev.responsive.kafka.internal.metrics.ClientVersionMetadata;
 import dev.responsive.kafka.internal.metrics.ResponsiveMetrics;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.common.metrics.Metrics;
-import org.apache.kafka.common.utils.MockTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class RS3TableFactoryTest {
   private final ResponsiveMetrics metrics = new ResponsiveMetrics(new Metrics());
   private ResponsiveMetrics.MetricScopeBuilder scopeBuilder;
 
+  @Mock
+  private GrpcRS3Client client;
 
   @BeforeEach
   public void setup() {
@@ -45,12 +72,16 @@ class RS3TableFactoryTest {
 
   @Test
   public void testTableMapping() {
-    var table = "test-table";
-    var uuid = "b1a45157-e2f0-4698-be0e-5bf3a9b8e9d1";
+    final var table = "test-table";
+    final var uuid = "b1a45157-e2f0-4698-be0e-5bf3a9b8e9d1";
+    final int partitions = 5;
 
-    var config = Mockito.mock(ResponsiveConfig.class);
-    Mockito.when(config.getMap(ResponsiveConfig.RS3_LOGICAL_STORE_MAPPING_CONFIG))
+    final var config = mock(ResponsiveConfig.class);
+    when(config.getMap(ResponsiveConfig.RS3_LOGICAL_STORE_MAPPING_CONFIG))
         .thenReturn(Collections.singletonMap(table, uuid));
+
+    when(client.createStore(any(UUID.class), anyInt(), any(CreateStoreOptions.class)))
+        .thenReturn(List.of(1, 2, 3, 4, 5));
 
     final RS3TableFactory factory = newTestFactory();
     final RS3KVTable rs3Table = (RS3KVTable) factory.kvTable(
@@ -59,16 +90,22 @@ class RS3TableFactoryTest {
         NO_TTL,
         metrics,
         scopeBuilder,
-        () -> 5
+        () -> partitions
     );
     assertEquals(uuid, rs3Table.storedId().toString());
+
+    verify(client).createStore(
+        UUID.fromString(uuid),
+        partitions,
+        new CreateStoreOptions(Optional.empty(), Optional.empty(), Optional.empty())
+    );
   }
 
   @Test
   public void testMissingTableMapping() {
-    String table = "test-table";
-    ResponsiveConfig config = Mockito.mock(ResponsiveConfig.class);
-    Mockito.when(config.getMap(ResponsiveConfig.RS3_LOGICAL_STORE_MAPPING_CONFIG))
+    final String table = "test-table";
+    final ResponsiveConfig config = mock(ResponsiveConfig.class);
+    when(config.getMap(ResponsiveConfig.RS3_LOGICAL_STORE_MAPPING_CONFIG))
         .thenReturn(Collections.emptyMap());
 
     final RS3TableFactory factory = newTestFactory();
@@ -79,7 +116,8 @@ class RS3TableFactoryTest {
   }
 
   private RS3TableFactory newTestFactory() {
-    final var connector = new GrpcRS3Client.Connector(new MockTime(), "localhost", 50051);
+    final var connector = mock(Connector.class);
+    lenient().when(connector.connect()).thenReturn(client);
     return new RS3TableFactory(connector);
   }
 
